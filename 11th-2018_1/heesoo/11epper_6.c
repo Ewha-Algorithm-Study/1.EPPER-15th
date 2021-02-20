a90067200b1be260433cdd0d4259e2faab7eb128
heesoo01 <73633272+heesoo01@users.noreply.github.com>
2021-02-20 20:16:10 +0900

    Update 11epper_6.c

--- 11th-2018_1/heesoo/11epper_6.c
+++ 11th-2018_1/heesoo/11epper_6.c
@@ -6,11 +6,53 @@ int main() {
 	int p = 0;
 	scanf("%d", &p);
 	int arr[6];
+	int bubble[6];
 	int pa = p;
+	
 	for (int i = 5;i >=0;i--) {
-		arr[i] = pa / pow(10, i);
+		bubble[i] = pa / pow(10, i);
 		pa = pa % (int)pow(10, i);
 	}
-	for (int i = 0;i < 6;i++)
-		printf("%d", arr[i]);
+	
+	for(int i=0;i<n;i++) {
+		arr[i]=bubble[i];
+		for (int j = 0;j < n - i - 1;j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = bubble[j];
+				bubble[j] = bubble[j + 1];
+				bubble[j + 1] = temp;
+			}
+		}
+			
+	//내림차순으로 수가 배열되어있으면 0을 출력하고 exit
+	int k=0;
+	while(){
+		if(arr[i]!=bubble[i])
+			break;
+		if(k==5){
+			printf("0");
+			exit(1);
+		}
+		k++;	
+	}
+	//뒷자리가 앞자리보다 크면 두 숫자를 바꾼 뒤, 나머지 숫자는 작은 숫자가 먼저 오게 배열한다.
+	int temp;
+	if(arr[0]>arr[1]){
+		temp=arr[0];
+		arr[0]=arr[1];
+		arr[1]=temp;
+		//버블정렬
+		for(int i=2;i<n;i++) 
+		for (int j = 0;j < n - i - 1;j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}else if{
+		
+	}
+	
+	
 }
